203759e58f9c299865c7dde5478eaf790406e57c
りんご飴196 <kazumac41@gmail.com>
2023-10-15 14:10:54 +0900

    複数対応

--- src/main/kotlin/com/github/Ringoame196/Shop.kt
+++ src/main/kotlin/com/github/Ringoame196/Shop.kt
@@ -1,6 +1,5 @@
 package com.github.Ringoame196
 
-import net.md_5.bungee.api.chat.hover.content.Item
 import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.Material
@@ -10,6 +9,7 @@ import org.bukkit.inventory.ItemStack
 
 class Shop {
     fun make(barrel: Barrel, player: org.bukkit.entity.Player, sign: Sign) {
+        if (player.world.name != "shop" && !player.isOp) { return }
         val item = player.inventory.itemInMainHand
         for (i in 0 until barrel.inventory.size) {
             val barrelItem = barrel.inventory.getItem(i)
@@ -18,7 +18,7 @@ class Shop {
             return
         }
         sign.setLine(0, "${ChatColor.GREEN}shop")
-        sign.setLine(1, if (sign.getLine(1) == "s") { "${ChatColor.RED}買い取り" } else { "${ChatColor.AQUA}販売" })
+        sign.setLine(1, if (sign.getLine(1) == "s") { "${ChatColor.RED}買取" } else { "${ChatColor.AQUA}販売" })
         sign.setLine(2, "${ChatColor.YELLOW}${sign.getLine(2)}円")
         val merchandise = if (item.itemMeta?.displayName != "") { item.itemMeta?.displayName } else { item.type }
         sign.setLine(3, "${ChatColor.GOLD}$merchandise")
@@ -31,17 +31,16 @@ class Shop {
         player.inventory.removeItem(playerItem)
         player.sendMessage("${ChatColor.AQUA}ショップを設置しました")
     }
-    fun sell(player: org.bukkit.entity.Player, barrel: Barrel, price: Int) {
-        val playerName = barrel.customName?.replace("@shop", "") ?: return
-        if ((Economy().get(playerName) ?: return) < price) {
+    fun sell(player: org.bukkit.entity.Player, barrel: Barrel, price: Int, count: Int) {
+        val playerName = barrel.customName?.replace("@shop", "")?.replace("@adminshop", "") ?: return
+        if ((Economy().get(playerName) ?: return) <= price) {
             Player().errorMessage(player, "${ChatColor.RED}買取不可")
             return
         }
-        if (Data().getBarrel(barrel.inventory) == 1728) {
+        if (Data().getBarrel(barrel.inventory) > (1728 - count)) {
             Player().errorMessage(player, "買取不可")
             return
         }
-        Economy().remove(playerName, price)
         val item = player.inventory.itemInMainHand
         val playerItem = item.clone()
         val merchandise = barrel.inventory.getItem(0)?.clone()
@@ -51,8 +50,12 @@ class Shop {
             Player().errorMessage(player, "アイテムが一致しませんでした")
             return
         }
-        barrel.inventory.addItem(playerItem)
+        playerItem.amount = count
+        if (!barrel.customName!!.contains("@adminshop")) {
+            barrel.inventory.addItem(playerItem)
+        }
         player.inventory.removeItem(playerItem)
+        Economy().remove(playerName, price)
         Economy().add(player.name, price)
     }
     fun purchase(player: org.bukkit.entity.Player, barrel: Barrel, price: Int) {
@@ -66,43 +69,47 @@ class Shop {
         }
         val gui = Bukkit.createInventory(null, 9, "${ChatColor.RED}ショップ購入:${barrel.location.x},${barrel.location.y},${barrel.location.z}")
         for (i in 0 until gui.size) {
-            gui.setItem(i, Item().make(Material.LIGHT_BLUE_STAINED_GLASS_PANE, " ", null, null))
+            gui.setItem(i, Item().make(Material.LIGHT_BLUE_STAINED_GLASS_PANE, " ", null, null, null))
         }
         val item = barrel.inventory.getItem(0)?.clone() ?: return
         item.amount = 1
         gui.setItem(1, item)
-        gui.setItem(4, Item().make(Material.EMERALD, "${ChatColor.GREEN}購入", "$price", null))
+        gui.setItem(4, Item().make(Material.EMERALD, "${ChatColor.GREEN}購入", "${price}円", null, 1))
+        gui.setItem(5, Item().make(Material.EMERALD, "${ChatColor.GREEN}購入", "${price}円", null, 32))
+        gui.setItem(6, Item().make(Material.EMERALD, "${ChatColor.GREEN}購入", "${price}円", null, 64))
         player.openInventory(gui)
     }
-    fun buy(player: org.bukkit.entity.Player, price: Int, barrel: Barrel) {
-        if (Data().getBarrel(barrel.inventory) == 1) {
+    fun buy(player: org.bukkit.entity.Player, price: Int, barrel: Barrel, count: Int) {
+        if (Data().getBarrel(barrel.inventory) <= count) {
             Player().errorMessage(player, "買取不可")
             player.closeInventory()
             return
         }
-        val playerName = barrel.customName?.replace("@shop", "") ?: return
+        val playerName = barrel.customName?.replace("@shop", "")?.replace("@adminshop", "") ?: return
         Economy().remove(player.name, price)
         Economy().add(playerName, price)
         val merchandise = barrel.inventory.getItem(0)?.clone()
-        merchandise?.amount = 1
-        barrelItemRemove(barrel)
+        merchandise?.amount = count
+        barrelItemRemove(barrel, count)
         player.inventory.addItem(merchandise)
     }
-    private fun barrelItemRemove(barrel: Barrel) {
+    private fun barrelItemRemove(barrel: Barrel, count: Int) {
         val barrelInventory = barrel.inventory
 
-        for (i in barrelInventory.size - 1 downTo 0) {
-            val item = barrelInventory.getItem(i)
-            if (item != null && item.type != Material.AIR) {
-                // 最後のアイテムを1つ減らす
-                val amount = item.amount
-                if (amount > 1) {
-                    item.amount = amount - 1
-                    break
-                } else {
-                    // アイテムが1つだけ残っている場合、空にする
-                    barrelInventory.setItem(i, ItemStack(Material.AIR))
-                    break
+        for (c in 0..count) {
+            for (i in barrelInventory.size - 1 downTo 0) {
+                val item = barrelInventory.getItem(i)
+                if (item != null && item.type != Material.AIR) {
+                    // 最後のアイテムを1つ減らす
+                    val amount = item.amount
+                    if (amount > 1) {
+                        item.amount = amount - 1
+                        break
+                    } else {
+                        // アイテムが1つだけ残っている場合、空にする
+                        barrelInventory.setItem(i, ItemStack(Material.AIR))
+                        break
+                    }
                 }
             }
         }
